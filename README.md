fddb77330f21a26fbd9c45641f97dc4ce48ff5cf
Clay McClure <clay@daemons.net>
2013-07-25 11:23:03 -0400

    Rename repo to cfping and add setup.py

---
+++ README.md
@@ -0,0 +1,31 @@
+cfping
+======
+
+Test the performance and availability of the Rackspace cloudfiles service.
+
+
+Example
+-------
+
+
+
+Usage
+-----
+
+
+
+Installation
+------------
+
+`cfping` is a Python program and can be installed with `pip` (or `easy_install`):
+
+    $ pip install cfping
+
+
+Contributing
+------------
+
+`cfping` is open-source software and your contributions are welcome.
+
+Open an [issue](https://github.com/claymation/cfping/issues) on GitHub to report a bug or suggest an enhancement,
+or better yet, fork the repo and send a [pull request](https://github.com/claymation/cfping/pulls).
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name = "cfping",
+    version = "0.2.0",
+    description = "Test the performance and availability of the Rackspace cloudfiles service.",
+    author = "Clay McClure",
+    author_email = "clay@daemons.net",
+    url = "https://github.com/claymation/cfping",
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Monitoring',
+    ],
+    scripts = ['cfping'],
+    install_requires = [
+        'python-cloudfiles',
+    ],
+)
